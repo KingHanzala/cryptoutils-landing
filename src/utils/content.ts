e5c14af3286c84c6f881765fd8261a2b9ad65a97
Hanzala <43098199+KingHanzala@users.noreply.github.com>
2025-05-24 12:13:07 +0530

    Add minilink

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,9 +1,16 @@
 export const initiatives = [
     {
+        name: "MiniLink",
+        about: "An open-source URL shortener with ability to manage links and track clicks.",
+        purpose: "To provide a platform for users to shorten their URLs and track clicks.",
+        link: "https://minilink.cryptoutils.xyz",
+        tag: "utility tool"
+    },
+    {
         name: "Solpe",
         about: "Merchant Payments App on Solana",
         purpose: "To provide a utility app which can provide an hassle free payment experience on Solana",
-        link: "https://solpe.cryptoutils.xyz/",
+        link: "https://solpe.cryptoutils.xyz",
         tag: "payments"
     },
     {
